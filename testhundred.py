cbe0de814f1e9d4fcc1c892131f173d2cade3290
tcx <472159011@qq.com>
2020-04-12 05:03:46 -0700

    2nd nnew

--- testhundred.py
+++ testhundred.py
@@ -46,13 +46,20 @@
 # print(name, country, language)
 
 
-basic_salary = 1500
-bonus_rate = 200
-commission_rate = 0.02
-numberofcamera = int(input("Enter the number of input sold:"))
-price = float(input("Enter the price of camera:"))
-bonus = (bonus_rate * numberofcamera)
-commission = (commission_rate * price * numberofcamera)
-print("Bonus = {:6.2f}".format(bonus))
-print("Commission = {:6.2f}".format(commission))
-print("Gross salary = {:6.2f}".format(basic_salary + bonus +commission))
+# basic_salary = 1500
+# bonus_rate = 200
+# commission_rate = 0.02
+# numberofcamera = int(input("Enter the number of input sold:"))
+# price = float(input("Enter the price of camera:"))
+# bonus = (bonus_rate * numberofcamera)
+# commission = (commission_rate * price * numberofcamera)
+# print("Bonus = {:6.2f}".format(bonus))
+# print("Commission = {:6.2f}".format(commission))
+# print("Gross salary = {:6.2f}".format(basic_salary + bonus +commission))
+
+
+# import math
+# area = 2 * 2 * math.pi
+# print("{:.10f}".format(area))
+
+
