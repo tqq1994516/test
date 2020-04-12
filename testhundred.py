2a28908c8243980918f17dee2a6a0f4cc30cd3ff
tcx <472159011@qq.com>
2020-04-12 04:33:17 -0700

    2nd new test

--- testhundred.py
+++ testhundred.py
@@ -41,7 +41,18 @@
 # a, b = 45, 54
 
 
-data = ("shiyanlou", "China", "Python")
-name, country ,language = data
-print(name, country, language)
+# data = ("shiyanlou", "China", "Python")
+# name, country ,language = data
+# print(name, country, language)
 
+
+basic_salary = 1500
+bonus_rate = 200
+commission_rate = 0.02
+numberofcamera = int(input("Enter the number of input sold:"))
+price = float(input("Enter the price of camera:"))
+bonus = (bonus_rate * numberofcamera)
+commission = (commission_rate * price * numberofcamera)
+print("Bonus = {:6.2f}".format(bonus))
+print("Commission = {:6.2f}".format(commission))
+print("Gross salary = {:6.2f}".format(basic_salary + bonus +commission))
